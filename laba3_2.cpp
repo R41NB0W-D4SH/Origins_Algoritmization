012aa08f35d1e9946d974079511ca896c97fd19d
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-15 13:40:54 +0300

    Update laba3_2.cpp

--- laba3_2.cpp
+++ laba3_2.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int b, i;
+	float n;
+	
+	cout << "Введите переменную B (B > 100 или B = 100)" << endl;
+	cin >> b;
+
+	n = 0;
+
+	if (b >= 100)
+	{
+		for (i = 100; i <= b; i++)
+		{
+			n = n + pow(i, 2);
+		}
+
+		b = (b - 100) + 1;
+		n = n / b;
+		cout << "Среднее арифметическое от 1 до 100 равно " << n << endl;
+	}
+	else
+	{
+		cout << "Вы ввели некорректную переменную B! Попробуйте еще раз." << endl;
+	}
+	
+	system("pause");
+	return 0;
+} 
