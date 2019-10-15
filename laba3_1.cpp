a9c9c06bfd208561a33304b0aaf001636c245c2f
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-15 13:21:35 +0300

    Update laba3_1.cpp

--- laba3_1.cpp
+++ laba3_1.cpp
@@ -6,20 +6,47 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	int n,i,nx;
+	int n, i, nx;
+	double b, c;
 
 	cout << "Введите целое число N " << endl;
 	cin >> n;
-	cout << "2 в степени " << n << " равно " << pow(2, n) << endl;
 
-	nx = 1;
-	for (i = 1; i <= n; i++)
+	c = 0;
+	if (n == 0)
+		{
+		nx = 0;
+		b = 0;
+		}
+	else
+		{
+		nx = 1;
+		b = 1;
+		}
+	if (n < 0)
 	{
-		nx = nx * i;
+		for (i = -1; i >= n; i--)
+		{
+			nx = nx * i;
+			b = b * (1 + 1 / pow(i, 2));
+			c = sqrt(c) + sqrt(2);
+		}
 	}
-	cout << "Факториал числа " << n << " равен " << nx << endl;
+	else if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			nx = nx * i;
+			b = b * (1 + 1 / pow(i, 2));
+			c = sqrt(c) + sqrt(2);
+		}
+	}
 
+	cout << "2 в степени " << n << " равно " << pow(2, n) << endl;
+	cout << "Факториал числа " << n << " равен " << nx << endl;
+	cout << "Результат произведения равен " << b << endl;
+	cout << "Результат корней равен " << c << endl;
 
 	system("pause");
 	return 0;
-}
+} 
