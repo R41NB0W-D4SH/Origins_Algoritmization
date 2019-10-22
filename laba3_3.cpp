c0fc785436e09ce10dba8e15f9af7b4068695c1e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-22 12:50:49 +0300

    Create laba3_3.cpp

---
+++ laba3_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int b, i, j, n;
+
+	cout << "Введите переменную N" << endl;
+	cin >> n;
+	b = 0;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			b = b + i;
+			cout << b;
+		}
+		b = 0;
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
