99f638e4e1504a88617e9fbe694f464b6e1df2f0
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-22 13:14:05 +0300

    Create laba4_1.cpp

---
+++ laba4_1.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int amount, sum, umn, n, i;
+	cout << "Введите размерность массива" << endl;
+	cin >> n;
+
+	amount = 0;
+	sum = 0;
+	umn = 1;
+	int *array = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> array[i];
+		if (array[i] & 2 == 1)
+		{
+			amount = amount++;
+			sum = sum + array[i];
+			umn = umn * array[i];
+		}
+	}
+
+	if (amount == 0)
+	{
+		umn = 0;
+	}
+
+	cout << "Сумма равна " << sum << endl << "Произведение равно " << umn << endl << " Количество равно " << amount << endl;
+	
+	system("pause");
+	return 0;
+}
+
