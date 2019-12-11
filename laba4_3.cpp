997d8ecd4de210165495729a3a31aae1383950a8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-12 01:34:37 +0300

    Update laba4_3.cpp

--- laba4_3.cpp
+++ laba4_3.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, j = 0, b;
+
+	cout << "Введите число B (элементы массива меньше этого числа будут удалены): ";
+	cin >> b;
+
+	cout << "Введите размерность массива: ";
+	cin >> n;
+
+	if (n > 0)
+	{
+		cout << endl << "Введите элементы массива." << endl << endl;
+
+		int *a = new int[n];
+
+		for (i = 0; i < n; i++)
+		{
+			cout <<"a[" << i << "] : ";
+			cin >> a[i];
+			if (a[i] >= b)
+			{
+				a[j] = a[i];
+				j++;
+			}
+		}
+
+		cout << endl << "Удаление элементов, не соответствующих условию:" << endl << endl;
+
+		for (i = 0; i < j; i++)
+		{
+			cout << "a[" << i << "] : " << a[i] << endl;
+		}
+	}
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
