2db372b3dd9cff87b06b465a31060578ae28545f
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-19 23:49:51 +0300

    Update laba5_3.cpp

--- laba5_3.cpp
+++ laba5_3.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, j, n, m, sum, pr;
+
+	cout << "Ввести размерность матрицы (N x M): ";
+	cin >> n >> m;
+
+	if (n > 0 & m > 0)
+	{
+		cout << endl << "Введите элементы массива" << endl << endl;
+
+		int **a = new int *[n];
+
+		for (i = 0; i < n; i++)
+		{
+		    a[i] = new int[m];
+			for (j = 0; j < m; j++)
+			{
+				cout << "a[" << i << "][" << j << "] : ";
+				cin >> a[i][j];
+			}
+		}
+
+		for (i = 0; i < n; i++)
+		{
+		    sum = 0; pr = 1;
+			for (j = 0; j < m; j++)
+			{
+                sum += a[i][j];
+                pr *= a[i][j];
+			}
+			cout << endl << sum << " " << pr;
+		}
+
+        cout << endl;
+	}
+	system("pause");
+	return 0;
+}
