27122945036dc79a30f0ceb0fa9d4ee06d4f975c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-21 21:51:39 +0300

    Update laba7_2.cpp

--- laba7_2.cpp
+++ laba7_2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string line;
+    char c;
+
+	cout << "Введите какую-нибудь строку: ";
+	getline(cin, line);
+
+	for (int i = 0; i < line.length(); i++)
+    {
+        if (isupper(line[i]))
+        {
+            line[i] = towlower(line[i]);
+        }
+    }
+
+	cout << endl << line << endl;
+	system("pause");
+	return 0;
+}
