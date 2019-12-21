7a7007776b6c0360ba837d8fa7847ba460f1fca4
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-21 21:24:29 +0300

    Update laba7_1.cpp

--- laba7_1.cpp
+++ laba7_1.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string line;
+
+	cout << "Введите какую-нибудь строку: ";
+	cin >> line;
+
+	cout << endl << "Длина, введенной строки: " << line.length() << endl;
+	system("pause");
+	return 0;
+}
