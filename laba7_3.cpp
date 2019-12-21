b9eae6e0b3b45bb489d32bfa2b28fc28ed5f702b
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-21 22:40:58 +0300

    Update laba7_3.cpp

--- laba7_3.cpp
+++ laba7_3.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string line1, line2;
+
+	cout << "Введите какую-нибудь строку: ";
+	getline(cin, line1);
+    cout << "Введите какую-нибудь строку: ";
+	getline(cin, line2);
+
+	if (line2.length() == line1.length())
+    {
+        for (int i = 0; i < line1.length(); i++)
+		{
+			if (tolower(line1[i]) != tolower(line2[i]))
+			{
+				cout << endl << "no" << endl;
+				return 0;
+			}
+		}
+    }
+    cout << endl << "yes" << endl << endl;
+	system("pause");
+	return 0;
+}
