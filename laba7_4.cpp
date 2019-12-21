eda97bf16192e21a0c96dc21dd138ea8c1acb26e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-21 23:24:23 +0300

    Update laba7_4.cpp

--- laba7_4.cpp
+++ laba7_4.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int i, j = 0;
+	string line, rewnd = "";
+
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	for (i = 0; i < line.length(); i++)
+	{
+		if (line[i] != ' ') rewnd += tolower(line[i]);
+	}
+
+	for (i = 0; i < rewnd.length() / 2; i++)
+	{
+		if (rewnd[i] != rewnd[rewnd.length() - i - 1])
+		{
+			cout << endl << "Строка не читается наоборот корректно!" << endl;
+			return 0;
+		}
+	}
+	cout << "Строка читаема наоборот (Строка - первертыш)!" << endl;
+	system("pause");
+	return 0;
+}
